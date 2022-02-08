2fd855fed79d13145934b7da85b829c89a3017d2
honghong <90856538+H-H-C@users.noreply.github.com>
2022-02-09 01:09:03 +0900

    hello world

--- Ex01_01.java
+++ Ex01_01.java
@@ -1,6 +1,5 @@
 public class Ex01_01 {
-	public static void main (String[] args){
-	
-System.out.print("Hello world");
+	public static void main(String[] args) {
+		System.out.print("Hello world");
 	}
 }
