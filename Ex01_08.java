9f989837f870559287c559aaee19090d923a03b7
honghong <90856538+H-H-C@users.noreply.github.com>
2022-02-09 01:12:43 +0900

    ex08

---
+++ Ex01_08.java
@@ -0,0 +1,15 @@
+public class Ex01_08 {
+	public static void main(String[] args) {
+
+		int su = 1234;
+
+		System.out.print("2?i???@");
+		System.out.println(su);
+		System.out.print("8?i???@");
+		System.out.println(su);
+		System.out.print("10?i???@");
+		System.out.println(su);
+		System.out.print("16?i???@");
+		System.out.println(su);
+	}
+}
