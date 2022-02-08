d481b60b0a960954dc2ec918c943b42bde4d7d1b
honghong <90856538+H-H-C@users.noreply.github.com>
2022-02-09 01:11:49 +0900

    ex02

---
+++ Ex01_02.java
@@ -0,0 +1,10 @@
+public class Ex01_02 {
+	public static void main (String[] args){
+	System.out.println("   *****");
+	System.out.println("  *     *");
+	System.out.println(" *       *");
+	System.out.println("***     ***");
+	System.out.println("  *     *  ");
+	System.out.println("  *******");
+	}
+	}
