f3f8220d34bd1cba4b531e67e3420c4934b0ca40
honghong <90856538+H-H-C@users.noreply.github.com>
2022-02-09 01:12:09 +0900

    ex04

---
+++ Ex01_04.java
@@ -0,0 +1,12 @@
+public class Ex01_04 {
+	public static void main(String[] args) {
+
+		int tanka = 500, suryo = 13, kingaku;
+
+		kingaku = tanka * suryo;
+
+		System.out.print("???z??");
+		System.out.print(kingaku);
+		System.out.print("????");
+	}
+}
