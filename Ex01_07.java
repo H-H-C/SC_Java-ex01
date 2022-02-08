44a139f5290b53351bbb2617419b55c7cadb41e7
honghong <90856538+H-H-C@users.noreply.github.com>
2022-02-09 01:12:34 +0900

    ex07

---
+++ Ex01_07.java
@@ -0,0 +1,18 @@
+public class Ex01_07 {
+	 public static void main(String[] args){
+	 
+	 	int su1 = 9876, su2 = 1234;
+	 	
+	 	
+	 	System.out.print("su1:");
+	 	System.out.print(su1);
+	 	System.out.print("su2:");
+	 	System.out.println(su2);
+	 	
+	 	System.out.println("˜a: " + su1 + su2);
+	 	System.out.println("·: " + (su1 - su2));
+	 	System.out.println("Ï: " + su1 * su2);
+	 	System.out.println("¤: " + su1 / su2);
+	 	System.out.println("è—]: " + su1 % su2);
+	 	}
+	 	}
